02a3f906328cff3529da5a18b49cbc6895ccad6d
dusty.irwin <dustin.irwin@icf.com>
2021-04-26 19:32:55 -0700

    removed < 400 winter therms, grouped into AB on > 20 therms saved per measure, 50% savings reduction  on both measures

--- MCL_2020_SHOP.jl
+++ MCL_2020_SHOP.jl
@@ -20,6 +20,9 @@ colnames = df_desc[!,:variable] .|> string
 prob_f = 0.005
 savings_reduction = 0.5  # applying 50% reduction on Tstat Therms Saved per McGee Y.
 
+# removing under 400 therms users
+df = @where(df, :WINTER_2020_THMS .> 400)
+
 # building cols
 @time begin
 
@@ -42,7 +45,7 @@ savings_reduction = 0.5  # applying 50% reduction on Tstat Therms Saved per McGe
     
     df[!,"HW/baseload"] .= df[!,"swh"] ./ df[!,"Baseload Therms"]
     
-    df[!,"WH Therms Saved"] .= df[!,"HW/baseload"] .* df[!,"Baseload Therms"]
+    df[!,"WH Therms Saved"] .= df[!,"HW/baseload"] .* df[!,"Baseload Therms"] * savings_reduction
     
     df[!,"Combined Therms Savings"] .= df[!,"Tstat Therms Saved"] .+ df[!,"WH Therms Saved"]
     
@@ -52,12 +55,27 @@ savings_reduction = 0.5  # applying 50% reduction on Tstat Therms Saved per McGe
     
 end
 
+# grouping
+df[!,"Group"] = [ 
+    if df[i,"Tstat Therms Saved"] > 20 && df[i,"WH Therms Saved"] > 20
+        "AB"
+    elseif df[i,"Tstat Therms Saved"] > 20
+        "A"
+    elseif df[i,"WH Therms Saved"] > 20
+        "B"
+    else
+        "C"  # zero cases
+    end for i in 1:length(df[!,:INDEX]) ]
+
+@where(df, :Group .== "AB" ) # 526235
+@where(df, :Group .== "A" ) # 28662
+@where(df, :Group .== "B" ) # 154033
+
 # writing and describing output data
 write("data/output/MCL_2020_SHOP-description.txt", df |> describe |> string)
-
 outfile = CSV.write("data/output/MCL_2020_SHOP.csv", df)
 
 # finding zeros in variables / possible NaN source
 sum(df[!,"2020_ANNUAL"] .== 0)
 sum(df[!,"Baseload Therms"] .== 0)
-sum(df[!,"Heating Therms"] .== 0)  # 2627 found
+sum(df[!,"Heating Therms"] .== 0)  # 74 found
